d8cfd64069507ac7558338d62b37d0f2b93de9d5
hemerson-git <48595787+hemerson-git@users.noreply.github.com>
2025-11-20 02:58:05 -0300

    feat: add vehicle routes initial

--- src/routes/vehicle.ts
+++ src/routes/vehicle.ts
@@ -1,33 +1,40 @@
 import { FastifyInstance } from "fastify";
 import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
 
 const prisma = new PrismaClient();
 
 export async function vehicleRoutes(app: FastifyInstance) {
-  app.get("/vehicles", async () => {
-    // const vehicles = await prisma.vehicle.findMany();
-    const vehicles = [
-      {
-        id: 1,
-        plate: "ABC-1234",
-        model: "Fusca",
-      },
-      {
-        id: 2,
-        plate: "XYZ-5678",
-        model: "Gol",
-      },
-    ];
+  app.get("/", async () => {
+    const vehicles = await prisma.veiculo.findMany();
+
     return vehicles;
   });
 
-  app.post("/vehicles", async (request, reply) => {
-    const body = request.body as { plate: string; model?: string };
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/",
+    {
+      schema: {
+        body: z.object({
+          placa: z
+            .string()
+            .max(7, "Placa inválida. Deve ter no máximo 7 caracteres.")
+            .min(7, "Placa inválida. Deve ter no mínimo 7 caracteres."),
+          modelo: z.string(),
+          cor: z.string(),
+          nome_cliente: z.string(),
+        }),
+      },
+    },
+    async (request, reply) => {
+      const data = request.body;
 
-    const created = await prisma.veiculo.create({
-      data: {},
-    });
+      const created = await prisma.veiculo.create({
+        data,
+      });
 
-    reply.code(201).send(created);
-  });
+      reply.code(201).send(created);
+    }
+  );
 }
